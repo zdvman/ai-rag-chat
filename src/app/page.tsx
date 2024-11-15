9d5db750990f828c7cbe56186f36f743251ccaa6
Dmytro Zuiev <zuyevdmytro@gmail.com>
2024-11-15 15:30:07 +0000

    second commit with all files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center items-center">
-      <div className="w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        <div className="mb-3 ml-1">
-          Enter a website URL as the route to get started collect data from the website for ai-chatbot
-          <Link className="block underline" href="http://localhost:3000/https://www.anthonynolan.org/help-save-a-life/join-stem-cell-register">
-            {"http://localhost:3000/https://www.anthonynolan.org/help-save-a-life/join-stem-cell-register"}
+    <main className='flex min-h-screen justify-center items-center'>
+      <div className='w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
+        <div className='mb-3 ml-1'>
+          Enter a website URL as the route to get started collect data from the
+          website for ai-chatbot
+          <Link
+            className='block underline'
+            href='https://ai-rag-chat.vercel.app//https://www.anthonynolan.org/help-save-a-life/join-stem-cell-register'
+          >
+            {
+              'http://localhost:3000/https://www.anthonynolan.org/help-save-a-life/join-stem-cell-register'
+            }
           </Link>
         </div>
       </div>
